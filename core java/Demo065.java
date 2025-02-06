fe04d24bcfaf4861286367549441626f16197c73
SAIPRAKASHREDDYM <saiprakashreddym5@gmail.com>
2025-02-06 11:40:44 +0530

    Elements of array

---
+++ core java/Demo065.java
@@ -0,0 +1,10 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        
+        System.out.println("Array elements:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
