144e439f12abaabb9474c0ea37bc665f47fc6ef1
SAIPRAKASHREDDYM <saiprakashreddym5@gmail.com>
2025-02-06 11:41:09 +0530

    Index values of array

---
+++ core java/Demo066.java
@@ -0,0 +1,11 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        
+        System.out.println("Array index and elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Index " + i + ": " + arr[i]);
+        }
+    }
+}
+
