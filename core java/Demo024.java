4b99f97f42c83c1409c43b9dff637338f5d5c4a4
SAIPRAKASHREDDYM <saiprakashreddym5@gmail.com>
2025-02-07 11:01:32 +0530

    Update and rename Demo025.java to Demo024.java

--- core java/Demo024.java
+++ core java/Demo024.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class demo025 {
+public class Demo024 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
