484e11ed762527431a27da1f8f53350cebb745e2
SAIPRAKASHREDDYM <saiprakashreddym5@gmail.com>
2025-02-07 14:52:02 +0530

    while loop

---
+++ core java/Demo006.java
@@ -0,0 +1,10 @@
+class Demo006{
+    public static void main(String args[]){
+        String username = "sai";
+        String pwd ="sai_123";
+        while(("sai".equals(username))&&("sai_123".equals(pwd))){
+            System.out.println("Hello Sai welcome to Malla Reddy university");
+            break;
+        }
+    }
+}
